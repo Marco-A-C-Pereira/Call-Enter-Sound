ba49e58b02da8a129ba42ae9a458daa659b16c03
unknown <finfokka@gmail.com>
2024-05-07 15:27:52 -0300

    Feature: Soundpad get sound list

--- soundpad.js
+++ soundpad.js
@@ -5,6 +5,7 @@ import { watchingStatus } from "./utils.js";
 export let soundpadPipeStatus = false;
 
 let client;
+let soundList;
 
 export function playSound() {
   // Cat Index, Sound Index, Play to Speakers, Play to Mic
@@ -19,12 +20,22 @@ async function createConnection() {
   return net.createConnection(pipePath, () => {
     console.log("Soundpad Pipe Ready");
     soundpadPipeStatus = true;
+    getSoundList();
   });
 }
 
 function addEvents() {
   client.on("data", (data) => {
-    console.log("Data recived:", data.toString());
+    if (data.toString() !== "R-200") {
+      let parsedData = JSON.parse(parser.toJson(data));
+      SoundList = parsedData.Soundlist.Sound;
+
+      SoundList.forEach((sound) => {
+        const { index, title, url } = sound;
+        console.log(fs.existsSync(url));
+        // console.log("Index: ", sound.index, " Name: ", sound.title);
+      });
+    }
   });
 
   client.on("end", () => {
@@ -49,6 +60,10 @@ function soundpadOperations() {
       client = await createConnection();
     }
   }, 250);
+}
+
+async function getSoundList() {
+  return client.write("GetSoundlist()");
 }
 
 soundpadOperations();
